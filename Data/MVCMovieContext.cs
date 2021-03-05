f1bd34e357c4157f399a0db5d050f420976e62ed
PopaBobaFeta <Iskand00@gmail.com>
2021-03-06 01:02:42 +0600

    asd

---
+++ Data/MVCMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using lab2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab2.Data
+{
+    public class MVCMovieContext : DbContext
+    {
+        public MVCMovieContext(DbContextOptions<MVCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
